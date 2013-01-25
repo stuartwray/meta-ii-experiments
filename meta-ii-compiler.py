5ea9a2511f5d7e6ed939421c65ff8b4e55982a68
Stuart Wray <scw@stuartwray.net>
2013-01-25 15:14:49 +0000

    Object (asm) code in compiler now corresponds to order of productions in original grammar.

--- meta-ii-compiler.py
+++ meta-ii-compiler.py
@@ -253,29 +253,181 @@ def END():
 PROG_TEXT = \
 """
 	ADR PROGRAM
-PROGRAM
-	TST '.SYNTAX'
+OUT1
+	TST '*1'
 	BF L1
-	ID
-	BE
-	CL 'ADR '
-	CI
-	OUT
-L2
-	CLL ST
-	BT L2
-	SET
-	BE
-	TST '.END'
-	BE
-	CL 'END'
+	CL 'GN1'
 	OUT
 L1
+	BT L2
+	TST '*2'
+	BF L3
+	CL 'GN2'
+	OUT
 L3
+	BT L2
+	TST '*'
+	BF L4
+	CL 'CI'
+	OUT
+L4
+	BT L2
+	SR
+	BF L5
+	CL 'CL '
+	CI
+	OUT
+L5
+L2
+	R
+OUTPUT
+	TST '.OUT'
+	BF L6
+	TST '('
+	BE
+L7
+	CLL OUT1
+	BT L7
+	SET
+	BE
+	TST ')'
+	BE
+L6
+	BT L8
+	TST '.LABEL'
+	BF L9
+	CL 'LB'
+	OUT
+	CLL OUT1
+	BE
+L9
+L8
+	BF L10
+	CL 'OUT'
+	OUT
+L10
+L11
+	R
+EX3
+	ID
+	BF L12
+	CL 'CLL '
+	CI
+	OUT
+L12
+	BT L13
+	SR
+	BF L14
+	CL 'TST '
+	CI
+	OUT
+L14
+	BT L13
+	TST '.ID'
+	BF L15
+	CL 'ID'
+	OUT
+L15
+	BT L13
+	TST '.NUMBER'
+	BF L16
+	CL 'NUM'
+	OUT
+L16
+	BT L13
+	TST '.STRING'
+	BF L17
+	CL 'SR'
+	OUT
+L17
+	BT L13
+	TST '('
+	BF L18
+	CLL EX1
+	BE
+	TST ')'
+	BE
+L18
+	BT L13
+	TST '.EMPTY'
+	BF L19
+	CL 'SET'
+	OUT
+L19
+	BT L13
+	TST '$'
+	BF L20
+	LB
+	GN1
+	OUT
+	CLL EX3
+	BE
+	CL 'BT '
+	GN1
+	OUT
+	CL 'SET'
+	OUT
+L20
+L13
+	R
+EX2
+	CLL EX3
+	BF L21
+	CL 'BF '
+	GN1
+	OUT
+L21
+	BT L22
+	CLL OUTPUT
+	BF L23
+L23
+L22
+	BF L24
+L25
+	CLL EX3
+	BF L26
+	CL 'BE'
+	OUT
+L26
+	BT L27
+	CLL OUTPUT
+	BF L28
+L28
+L27
+	BT L25
+	SET
+	BE
+	LB
+	GN1
+	OUT
+L24
+L29
+	R
+EX1
+	CLL EX2
+	BF L30
+L31
+	TST '/'
+	BF L32
+	CL 'BT '
+	GN1
+	OUT
+	CLL EX2
+	BE
+L32
+L33
+	BT L31
+	SET
+	BE
+	LB
+	GN1
+	OUT
+L30
+L34
 	R
 ST
 	ID
-	BF L4
+	BF L35
 	LB
 	CI
 	OUT
@@ -287,180 +439,28 @@ ST
 	BE
 	CL 'R'
 	OUT
-L4
-L5
-	R
-EX1
-	CLL EX2
-	BF L6
-L7
-	TST '/'
-	BF L8
-	CL 'BT '
-	GN1
-	OUT
-	CLL EX2
-	BE
-L8
-L9
-	BT L7
-	SET
-	BE
-	LB
-	GN1
-	OUT
-L6
-L10
-	R
-EX2
-	CLL EX3
-	BF L11
-	CL 'BF '
-	GN1
-	OUT
-L11
-	BT L12
-	CLL OUTPUT
-	BF L13
-L13
-L12
-	BF L14
-L15
-	CLL EX3
-	BF L16
-	CL 'BE'
-	OUT
-L16
-	BT L17
-	CLL OUTPUT
-	BF L18
-L18
-L17
-	BT L15
-	SET
-	BE
-	LB
-	GN1
-	OUT
-L14
-L19
-	R
-EX3
-	ID
-	BF L20
-	CL 'CLL '
-	CI
-	OUT
-L20
-	BT L21
-	SR
-	BF L22
-	CL 'TST '
-	CI
-	OUT
-L22
-	BT L21
-	TST '.ID'
-	BF L23
-	CL 'ID'
-	OUT
-L23
-	BT L21
-	TST '.NUMBER'
-	BF L24
-	CL 'NUM'
-	OUT
-L24
-	BT L21
-	TST '.STRING'
-	BF L25
-	CL 'SR'
-	OUT
-L25
-	BT L21
-	TST '('
-	BF L26
-	CLL EX1
-	BE
-	TST ')'
-	BE
-L26
-	BT L21
-	TST '.EMPTY'
-	BF L27
-	CL 'SET'
-	OUT
-L27
-	BT L21
-	TST '$'
-	BF L28
-	LB
-	GN1
-	OUT
-	CLL EX3
-	BE
-	CL 'BT '
-	GN1
-	OUT
-	CL 'SET'
-	OUT
-L28
-L21
-	R
-OUTPUT
-	TST '.OUT'
-	BF L29
-	TST '('
-	BE
-L30
-	CLL OUT1
-	BT L30
-	SET
-	BE
-	TST ')'
-	BE
-L29
-	BT L31
-	TST '.LABEL'
-	BF L32
-	CL 'LB'
-	OUT
-	CLL OUT1
-	BE
-L32
-L31
-	BF L33
-	CL 'OUT'
-	OUT
-L33
-L34
-	R
-OUT1
-	TST '*1'
-	BF L35
-	CL 'GN1'
-	OUT
 L35
-	BT L36
-	TST '*2'
+L36
+	R
+PROGRAM
+	TST '.SYNTAX'
 	BF L37
-	CL 'GN2'
-	OUT
-L37
-	BT L36
-	TST '*'
-	BF L38
-	CL 'CI'
+	ID
+	BE
+	CL 'ADR '
+	CI
 	OUT
 L38
-	BT L36
-	SR
-	BF L39
-	CL 'CL '
-	CI
+	CLL ST
+	BT L38
+	SET
+	BE
+	TST '.END'
+	BE
+	CL 'END'
 	OUT
+L37
 L39
-L36
 	R
 	END
 """
